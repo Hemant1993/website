8f0b2eff91950a9953397d22289db061bacfd998
Hemant Arora <hemantarora1993@gmail.com>
2018-05-16 13:22:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,497 @@
+function mouseoverbox1(){
+	var mypara = document.getElementById("nav1");
+	mypara.style.color = "#377037";
+}
+function mouseoutbox1(){
+	var mypara = document.getElementById("nav1");
+	mypara.style.color = "black";
+}
+function mouseoverbox2(){
+	var mypara = document.getElementById("nav1");
+	var mypara2 = document.getElementById("one");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox2(){
+	var mypara2 = document.getElementById("one");
+	mypara2.style.color = "black";
+}
+function mouseoverbox3(){
+	var mypara = document.getElementById("nav1");
+	var mypara2 = document.getElementById("two");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox3(){
+	var mypara2 = document.getElementById("two");
+	mypara2.style.color = "black";
+}
+function mouseoverbox4(){
+	var mypara = document.getElementById("nav1");
+	var mypara2 = document.getElementById("three");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox4(){
+	var mypara2 = document.getElementById("three");
+	mypara2.style.color = "black";
+}
+function mouseoverbox5(){
+	var mypara = document.getElementById("nav1");
+	var mypara2 = document.getElementById("four");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox5(){
+	var mypara2 = document.getElementById("four");
+	mypara2.style.color = "black";
+}
+function mouseoverbox6(){
+	var mypara = document.getElementById("nav1");
+	var mypara2 = document.getElementById("five");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox6(){
+	var mypara2 = document.getElementById("five");
+	mypara2.style.color = "black";
+}
+
+function mouseoverbox7(){
+	var mypara = document.getElementById("nav1");
+	var mypara2 = document.getElementById("six");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox7(){
+	var mypara2 = document.getElementById("six");
+	mypara2.style.color = "black";
+}
+function mouseoverbox8(){
+	var mypara = document.getElementById("nav2");
+	mypara.style.color = "#377037";
+}
+function mouseoutbox8(){
+	var mypara2 = document.getElementById("nav2");
+	mypara2.style.color = "black";
+}
+function mouseoverbox9(){
+	var mypara = document.getElementById("nav2");
+	var mypara2 = document.getElementById("seven");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox9(){
+	var mypara2 = document.getElementById("seven");
+	mypara2.style.color = "black";
+}
+function mouseoverbox10(){
+	var mypara = document.getElementById("nav2");
+	var mypara2 = document.getElementById("eight");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox10(){
+	var mypara2 = document.getElementById("eight");
+	mypara2.style.color = "black";
+}
+function mouseoverbox11(){
+	var mypara = document.getElementById("nav2");
+	var mypara2 = document.getElementById("nine");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox11(){
+	var mypara2 = document.getElementById("nine");
+	mypara2.style.color = "black";
+}
+function mouseoverbox12(){
+	var mypara = document.getElementById("nav2");
+	var mypara2 = document.getElementById("ten");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox12(){
+	var mypara2 = document.getElementById("ten");
+	mypara2.style.color = "black";
+}
+function mouseoverbox13(){
+	var mypara = document.getElementById("nav2");
+	var mypara2 = document.getElementById("eleven");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox13(){
+	var mypara2 = document.getElementById("eleven");
+	mypara2.style.color = "black";
+}
+function mouseoverbox14(){
+	var mypara = document.getElementById("nav2");
+	var mypara2 = document.getElementById("twelve");
+	mypara.style.color = "#377037";
+	mypara2.style.color = "#377037";
+}
+function mouseoutbox14(){
+	var mypara2 = document.getElementById("twelve");
+	mypara2.style.color = "black";
+}
+function slide(){
+	var mypara = document.getElementById("slide");
+	setTimeout(function(){
+		mypara.style.marginLeft = "1px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "2px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "3px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "4px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "5px";
+	},250);
+}
+function slideout(){
+	var mypara = document.getElementById("slide");
+	setTimeout(function(){
+		mypara.style.marginLeft = "4px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "3px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "2px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "1px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "0px";
+	},250);
+}
+function slide2(){
+	var mypara = document.getElementById("slide2");
+	setTimeout(function(){
+		mypara.style.marginLeft = "131px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "132px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "133px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "134px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "135px";
+	},250);
+}
+function slideout2(){
+	var mypara = document.getElementById("slide2");
+	setTimeout(function(){
+		mypara.style.marginLeft = "134px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "133px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "132px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "131px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "130px";
+	},250);
+}
+function slide3(){
+	var mypara = document.getElementById("slide3");
+	setTimeout(function(){
+		mypara.style.marginLeft = "21px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "22px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "23px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "24px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "25px";
+	},250);
+}
+function slideout3(){
+	var mypara = document.getElementById("slide3");
+	setTimeout(function(){
+		mypara.style.marginLeft = "24px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "23px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "22px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "21px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "20px";
+	},250);
+}
+function slide4(){
+	var mypara = document.getElementById("slide4");
+	setTimeout(function(){
+		mypara.style.marginLeft = "21px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "22px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "23px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "24px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "25px";
+	},250);
+}
+function slideout4(){
+	var mypara = document.getElementById("slide4");
+	setTimeout(function(){
+		mypara.style.marginLeft = "24px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "23px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "22px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "21px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "20px";
+	},250);
+}
+function slide5(){
+	var mypara = document.getElementById("slide5");
+	setTimeout(function(){
+		mypara.style.marginLeft = "21px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "22px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "23px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "24px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "25px";
+	},250);
+}
+function slideout5(){
+	var mypara = document.getElementById("slide5");
+	setTimeout(function(){
+		mypara.style.marginLeft = "24px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "23px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "22px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "21px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "20px";
+	},250);
+}
+function slide6(){
+	var mypara = document.getElementById("slide6");
+	setTimeout(function(){
+		mypara.style.marginLeft = "1px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "2px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "3px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "4px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "5px";
+	},250);
+}
+function slideout6(){
+	var mypara = document.getElementById("slide6");
+	setTimeout(function(){
+		mypara.style.marginLeft = "4px";
+	},50);
+	setTimeout(function(){
+		mypara.style.marginLeft = "3px";
+	},100);
+	setTimeout(function(){
+		mypara.style.marginLeft = "2px";
+	},150);
+	setTimeout(function(){
+		mypara.style.marginLeft = "1px";
+	},200);
+	setTimeout(function(){
+		mypara.style.marginLeft = "0px";
+	},250);
+}
+function left(){
+		var mypara = document.getElementById("left");
+		mypara.style.backgroundColor = "transparent";
+		var mypara = document.getElementById("right");
+		mypara.style.backgroundColor = "#216C55";
+		document.getElementById("left").disabled = true;
+		document.getElementById("right").disabled = false;
+		var mypara2 = document.getElementById("slider");
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-30px";
+		},50);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-60px";
+		},100);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-90px";
+		},150);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-120px";
+		},200);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-150px";
+		},250);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-180px";
+		},300);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-210px";
+		},350);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-240px";
+		},400);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-270px";
+		},450);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-300px";
+		},500);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-330px";
+		},550);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-360px";
+		},600);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-390px";
+		},650);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-420px";
+		},700);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-450px";
+		},750);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-480px";
+		},800);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-510px";
+		},850);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-540px";
+		},900);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-570px";
+		},950);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-600px";
+		},1000);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-630px";
+		},1050);
+}
+function right(){
+		var mypara = document.getElementById("right");
+		mypara.style.backgroundColor = "transparent";
+		var mypara = document.getElementById("left");
+		mypara.style.backgroundColor = "#216C55";
+		document.getElementById("right").disabled = true;
+		document.getElementById("left").disabled = false;
+		var mypara2 = document.getElementById("slider");
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-600px";
+		},50);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-570px";
+		},100);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-540px";
+		},150);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-510px";
+		},200);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-480px";
+		},250);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-450px";
+		},300);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-420px";
+		},350);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-390px";
+		},400);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-360px";
+		},450);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-330px";
+		},500);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-300px";
+		},550);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-270px";
+		},600);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-240px";
+		},650);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-210px";
+		},700);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-180px";
+		},750);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-150px";
+		},800);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-120px";
+		},850);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-90px";
+		},900);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-60px";
+		},950);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "-30px";
+		},1000);
+		setTimeout(function(){
+			mypara2.style.marginLeft = "0px";
+		},1050);
+}
